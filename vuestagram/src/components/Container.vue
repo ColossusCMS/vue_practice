183b5617f7df5b94feeaff5fea7efa5bfd9f8d59
Mun-seok CHOI <yaahq123@gmail.com>
2021-09-27 18:00:19 +0900

    인스타그램 클론 시작

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post v-for="(data, i) in postData" :key="i" :data="postData[i]" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: 'Container',
+  components: {
+    Post: Post,
+  },
+  props: {
+    postData: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
